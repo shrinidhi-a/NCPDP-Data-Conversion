91816b4307adf9a83e3f14f4dd6f5606eef5cb15
Fizan <mohammedfizan08@gmail.com>
2025-05-27 16:12:00 +0530

    Implementation: Fixed the env and code is perfectly running

--- mango_db/app/Database/connection.py
+++
@@ -1,7 +0,0 @@
-import motor.motor_asyncio
-from bson.errors import InvalidId
-from bson import ObjectId
-
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
-db = client.healthcare
-patients_collection = db.patients_data
--- mango_db/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.router import patients  # Make sure to import the patients router correctly
-from app.router import nested_resources  # Import the nested resources router
-
-app = FastAPI()
-
-# Include the router in the main app
-app.include_router(patients.router)
-app.include_router(nested_resources.router)   # Include the nested resources router
